679b6ff9016a69c4024f7bb84d1cf72156037fe8
Chris Pynegar <chris@chrispynegar.co.uk>
2016-07-12 10:09:46 +0100

    Auth config

---
+++ config/auth.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /**
+     * The URL to redirect to when authentication is requested.
+     */
+    'login_url' => 'login',
+
+    /**
+     * The URL to redirect to when the user logs out.
+     */
+    'logout_url' => '',
+
+    /**
+     * The username field.
+     */
+    'username' => 'email',
+
+    /**
+     * The password field.
+     */
+    'password' => 'password'
+
+];
